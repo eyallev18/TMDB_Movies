22c3fe10ef54a827c98b25d3b4ace4718885ef39
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-16 16:10:22 +0200

    Updates

---
+++ precache-manifest.6a0356ae58234b3f62e98a49a3430bd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4130529123f3db3644c37486f6ab2a7",
+    "url": "/TMDB_Movies/index.html"
+  },
+  {
+    "revision": "dcdb0173e9183de766f8",
+    "url": "/TMDB_Movies/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "091af8b7edf5e19f1269",
+    "url": "/TMDB_Movies/static/css/main.ff528508.chunk.css"
+  },
+  {
+    "revision": "dcdb0173e9183de766f8",
+    "url": "/TMDB_Movies/static/js/2.6bdff176.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/TMDB_Movies/static/js/2.6bdff176.chunk.js.LICENSE"
+  },
+  {
+    "revision": "091af8b7edf5e19f1269",
+    "url": "/TMDB_Movies/static/js/main.a0ab7bd2.chunk.js"
+  },
+  {
+    "revision": "c1b5d1b1cbfe7ab6fe4e",
+    "url": "/TMDB_Movies/static/js/runtime-main.a4023ea5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TMDB_Movies/static/media/logo.5d5d9eef.svg"
+  }
+]);
